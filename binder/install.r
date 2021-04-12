f052121dc22adbd2e324d543f5291057f87338bc
Kimon Spiliopoulos <KSpiliop@users.noreply.github.com>
2021-04-12 18:28:03 +0300

    Create install.r

---
+++ binder/install.r
@@ -0,0 +1,9 @@
+### install regular packages
+
+install.packages("reticulate") # python support in RMarkdown
+install.packages("ggplot2") # for plotting
+install.packages(c("tm", "topicmodels", "tidytext","dplyr")) 
+
+### install GitHub packages (tag = commit, branch or release tag)
+# install.packages("devtools")
+# devtools::install_github("user/repo", ref = "tag")
